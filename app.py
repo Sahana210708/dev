a7a4e8236803c4630078fa22ac3913a59785b8ca
Sahana210708 <sahana230706@gmail.com>
2025-12-27 08:11:17 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from flask import Flask,render_template,request
+import google.generativeai as genai
+from dotenv  import load_dotenv
+import os
+
+load_dotenv()
+
+app=Flask(__name__)
+genai.configure(api_key=os.getenv("AIzaSyBgExVg2VFs04PHvI_q2zAVsKedLZhJnTM"))
+model=genai.GenerativeModel("gemini -2.5-flash")
+df=pd.read_csv("qa_data (1) (1).csv")
+
+context_texts=""
+for _,row in df.iterrows():
+    context_texts +=f"Q:{row['question']}\nA:{row['answer']}\n\n"
+def ask_gemini(query):
+    prompt=f""" 
+    you are a Q&A assistant. 
+    Answer ONLY using the context below.
+    if the answer if not present,say:no relevant Q&A found
+
+    context:
+    {context_texts}
+    question:{query}
+"""
+    response= model.generate_content(prompt)
+    return response.text.strip()
+
+@app.route("/",methods=["GET","POST"])
+def home():
+    answer=""
+    if request.method=="POST":
+        query=request.form["query"]
+        answer=ask_gemini(query)
+    return render_template("index.html",answer=answer)
+if __name__ =="__main__":
+    app.run()
